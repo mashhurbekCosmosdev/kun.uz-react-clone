7edbe8619921569ea2a08dacb57e1d46ba0cb07b
mashhurbekCosmosdev <139423413+mashhurbekCosmosdev@users.noreply.github.com>
2024-01-07 23:14:42 +0500

    github go

--- src/Home/home-two/home.jsx
+++ src/Home/home-two/home.jsx
@@ -32,7 +32,7 @@ function Home() {
         <div className="put">
         <div className="pivture_next_div1"><img src={img1}/></div>
         <div className="pivture_next_div2"><br />
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30</h4><br />
         <h2>Uzbekistan plans to launch 24 major projects worth $12.7 billion in electrical, chemical and pharmaceutical industries next year</h2><br />
         <p>On December 25, President Shavkat Mirziyoyev chaired a meeting on the development of the electrical, chemical and pharmaceutical industries in Uzbekistan.</p>
         </div>
@@ -42,17 +42,17 @@ function Home() {
         <div className="smal_picture_and_div1">
         <div className="picture_img1"><img src={img2}/></div>
         <div className="picture_img2">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
         <h3>Aziz Voitov suspected of embezzlement and abuse of official power </h3><br /><br />
         <hr className="hr"/>
         </div>
         </div>
         <div className="smal_picture_and_div2">
         <div className="smal_picture_and_div1">
-        <div className="picture_img1"><img src={img3}/></div>
-        <div className="picture_img2">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
-        <h3>Saida Mirziyoyeva speaks about reform of the national alphabet</h3><br /><br /><br />
+        <div className="picture_img3"><img src={img3}/></div>
+        <div className="picture_img4">
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
+        <h3>Saida Mirziyoyeva speaks about reform of the national alphabet</h3><br /><br />
         <hr className="hr"/>
         
         </div>
@@ -64,17 +64,17 @@ function Home() {
         <div className="smal_picture_and_div1">
         <div className="picture_img1"><img src={img2}/></div>
         <div className="picture_img2">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
-        <h3>Aziz Voitov suspected of embezzlement and abuse of official power </h3><br /><br />
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
+        <h3>Aziz Voitov suspected of official of embezzlementof official and abuse of official power </h3><br />
         <hr className="hr"/>
         </div>
         </div>
         <div className="smal_picture_and_div2">
         <div className="smal_picture_and_div1">
-        <div className="picture_img1"><img src={img3}/></div>
-        <div className="picture_img2">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
-        <h3>Saida Mirziyoyeva speaks about reform of the national alphabet</h3><br /><br /><br />
+        <div className="picture_img3"><img src={img3}/></div>
+        <div className="picture_img4">
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
+        <h3>Saida Mirziyoyeva speaks about reform of the national alphabet</h3><br /><br />
         <hr className="hr"/>
         </div>
         </div>
@@ -89,22 +89,22 @@ function Home() {
         <div className="three_picture">
         <div className="one_picture"><img src={img4}/></div>
         <div className="one_message">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
         <h3>Draft law: Powers to conduct searches, wiretapping and seizure of property will be transferred from prosecutors to courts</h3>
         </div>
         </div>
         <div className="three_picture">
         <div className="one_picture"><img src={img5}/></div>
         <div className="one_message">
-        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4>
+        <h4><box-icon name='box' color='#737373' ></box-icon>15:30 / 25.12.2023</h4><br />
         <h3>Gold bar prices on the rise in Uzbekistan</h3>
         </div>
         </div>
         <div className="three_picture">
         <div className="one_picture"><img src={img6}/></div>       
         <div className="one_message">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30 / 25.12.2023</h4>
-        <h3>Central Bank opposes P2P control</h3>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30 / 25.12.2023</h4><br />
+        <h3>Central Bank opposes P2P control </h3>
         </div>
         </div>
         </div>
@@ -114,43 +114,43 @@ function Home() {
         </div>
         <div className="container_center2">
         <div className="left_end_divs1">
-        <h1><box-icon name='disc' type='solid' color='#0a0b56' ></box-icon>Latest news</h1><br />
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h1><box-icon name='disc' type='solid' color='#0a0b56' ></box-icon>Latest news</h1>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
         <div className="left_end_divs">
-        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4>
+        <h4><box-icon name='box' color='#737373'></box-icon>15:30</h4><br />
         <h3>Uzbekistan may stop producing AI-80 gasoline by 2026</h3>
         <hr className="hr"/>
         </div>
